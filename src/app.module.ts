61546a7c9e7b86204a28f18b2584b7ac23f19cb0
yewinag <yewinaung831@gmail.com>
2023-02-27 22:32:45 +0700

    creat user crud

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { PostsModule } from './posts/posts.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
 import { CommentsModule } from './comments/comments.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
@@ -15,6 +16,7 @@ import { CommentsModule } from './comments/comments.module';
     MongooseModule.forRoot(process.env.DB_URI_LOCAL),
     PostsModule,
     CommentsModule,
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
