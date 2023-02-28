aa849cbaa5e12d5ed393d6622eb29dc47c3c3bfe
yewinag <yewinaung831@gmail.com>
2023-02-28 22:42:06 +0700

    fix prefix value

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('api');
+  // app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT || 3000);
 }
